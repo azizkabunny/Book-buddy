45cd1294a8ae3d58a7c6293bb288ebff9e0ce867
aziza <azizamakhkamova88@gmail.com>
2026-09-08 00:28:22 -0600

    all routes created. components added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Catalog from './Catalog';
+import BookDetails from './BookDetails';
+import Account from './Account';
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Catalog />} />
+        <Route path='/books' element={<Catalog />} />
+        <Route path='/books/:id' element={<BookDetails />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
+    </>
+  );
 }
